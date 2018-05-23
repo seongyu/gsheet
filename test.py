4dcbedf876867316a7571cd4c0b809333ed7a572
LeonKim <leonkim@LeonKimui-MacBook-Pro.local>
2018-05-23 18:45:14 +0900

    test

--- test.py
+++ test.py
@@ -2,22 +2,23 @@
 import config, json
 from flask import Flask, request
 import lib.mysql as db
-#import lib.calendar as calendar
+import calendar_updater as cu
 
 app = Flask(__name__)
 
-@app.route('/',methods=['post'])
+@app.route('/u',methods=['post'])
 def get_data():
-  data = request.form['rows']
-  print(data)
+  data = request.form['rows'] # [1,2,3,4]  
   return response(data)
 
+@app.route('/ua',methods=['post'])
+def get_data():
+  cu.get_sheet_data(config.RESERVE_SHEET)
+  return response('')
 
 def response(data):
   res = {'status':'OK','data':data}
   return json.dumps(res)
 
-
 if __name__ == '__main__':
-  # db.testFn()
   app.run(host='0.0.0.0', port='8080')
