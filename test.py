984b31456acfd5c71b0f431494fb60c5c3a8107e
LeonKim <leonkim@LeonKimui-MacBook-Pro.local>
2018-05-23 17:38:08 +0900

    test add

--- test.py
+++ test.py
@@ -1,20 +1,30 @@
 #custom imports
 import config, json
 from flask import Flask, request
+import lib.mysql as db
 #import lib.calendar as calendar
+
+
+def testFn():
+  (db, cursor) = _connect()
+  stmt = 'select * from test;';
+  cursor.execute(stmt)
+  rows = cursor.fetchall()
+  db.close()
 
 app = Flask(__name__)
 
 @app.route('/',methods=['post'])
 def get_data():
-    data = request.form
-    return response(data)
+  data = request.form
+  return response(data)
 
 
 def response(data):
-    res = {'status':'OK','data':data}
-    return json.dumps(res)
+  res = {'status':'OK','data':data}
+  return json.dumps(res)
 
 
 if __name__ == '__main__':
+  testFn()
   app.run(host='0.0.0.0', port='5000')
